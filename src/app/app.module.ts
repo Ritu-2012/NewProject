9780385b3dc07122bdc9666b1ea7cef881fd9e15
Ritu Singh <rit.singh2012@gmail.com>
2020-10-06 20:02:56 +0530

    Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponentComponent } from './child-component/child-component.component';
+import { ParentComponentComponent } from './parent-component/parent-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponentComponent,
+    ParentComponentComponent
   ],
   imports: [
     BrowserModule,
